ec9cda99d3222d11ec852c21be3031bb509ada5a
Brian Pham <brianlpham21@gmail.com>
2018-03-08 18:27:57 -0800

    completed second section udemy react

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,16 +5,20 @@ export default class SearchBar extends Component {
     super(props);
 
     this.state = {
-      term: 'Starting Value'
+      term: ''
     };
   }
 
   render() {
     return (
-      <div>
-        <input value={this.state.term} onChange={event => this.setState({term: event.target.value})} />
-        <p>Value of the input: {this.state.term}</p>
+      <div className='search-bar'>
+        <input value={this.state.term} onChange={event => this.onInputChange(event.target.value)} />
       </div>
     )
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 };
